077ae75cc16ecb5f83f97562b84cc88daafcdc82
4a4c <4a4c@users.noreply.github.com>
2016-09-10 22:13:27 +0000

    clone networkboot

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ NEW_HOSTNAME=bootstrap
 echo "setting hostname to ${NEW_HOSTNAME}"
 sudo hostnamectl set-hostname ${NEW_HOSTNAME}
 
+#clone network boot repo
+mkdir -p networkboot
+git clone https://github.com/networkboot/docker-dhcpd ./networkboot/docker-dhcp/
