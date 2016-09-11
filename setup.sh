7edf214f26db7c204c6d157812c47a072f547579
4a4c <4a4c@users.noreply.github.com>
2016-09-11 00:24:02 +0000

    build tftp container

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 NEW_HOSTNAME=bootstrap
 
 #set hostname
-echo "setting hostname to ${NEW_HOSTNAME}"
+echo "== setting hostname to ${NEW_HOSTNAME} =="
 sudo hostnamectl set-hostname ${NEW_HOSTNAME}
 
 #set static ip
-echo "setting static ip"
-echo "please re-run this setup script after rebooting the machine and logging back in"
+echo "== setting static ip =="
+echo "== please re-run this setup script after rebooting the machine and logging back in =="
 if [ ! -f /etc/systemd/network/10-static.network ]; then
 	sudo cp ./includes/10-static.network /etc/systemd/network/
 	sudo systemctl restart systemd-networkd
@@ -15,21 +15,25 @@ if [ ! -f /etc/systemd/network/10-static.network ]; then
 fi
 
 #clone network boot repo
-echo "cloning networkboot/docker-dhcp repo"
+echo "== cloning networkboot/docker-dhcp repo =="
 if [ ! -d "networkboot" ]; then
 	mkdir -p networkboot
 	git clone https://github.com/networkboot/docker-dhcpd ./networkboot/docker-dhcp/
 fi
 
 #build dhcpd container
-echo "building docker-dhcp container"
+echo "== building docker-dhcp container =="
 ./networkboot/docker-dhcp/build
 
 #setup dhcpd service
-echo "setting up dhcpd service"
+echo "== setting up dhcpd service =="
 if [ ! -f /etc/systemd/system/dhcpd.service ]; then
 	sudo cp ./includes/dhcpd.service /etc/systemd/system/
 	sudo systemctl enable dhcpd
 	sudo systemctl start dhcpd
 fi
 systemctl status dhcpd
+
+#build tftpd container
+echo "== building tftpd container =="
+docker build -t 4a4c/tftpd ./tftp/
