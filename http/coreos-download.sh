d75ededb1b3ba2845037bd303607f173f76d856c
4a4c <4a4c@users.noreply.github.com>
2016-09-12 01:39:00 +0000

    set BASE_DIR

--- http/coreos-download.sh
+++ http/coreos-download.sh
@@ -1,8 +1,11 @@
-# tail -n +2 reads all lines starting on 2 (excluders headers)
+# global vars
 # dirname command is using script dir regardless of where it is executed from
+BASE_DIR=$(dirname -- "$0")
+
+# tail -n +2 reads all lines starting on 2 (excluders headers)
 
 # loop through builds file, for each line download
-tail -n +2 $(dirname -- "$0")/coreos-builds.csv | while read line
+tail -n +2 $BASE_DIR/coreos-builds.csv | while read line
   do 
     # set some vars
     CURRENT_LINE=$(echo $line)
@@ -13,8 +16,8 @@ tail -n +2 $(dirname -- "$0")/coreos-builds.csv | while read line
     echo "channel is ${CHANNEL} build is ${BUILD}"
     
     # ensure download dir exists
-    mkdir -p $(dirname -- "$0")/httpd/coreos/$CHANNEL
+    mkdir -p $BASE_DIR/httpd/coreos/$CHANNEL
 
     # check for files and download if they don't exist
-    
+    #if [ !f ]
   done
