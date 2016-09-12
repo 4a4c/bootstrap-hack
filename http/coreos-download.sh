0bef3b081824d099ed25ebdb261c46383e62c5f4
4a4c <4a4c@users.noreply.github.com>
2016-09-12 02:18:37 +0000

    make download more intel

--- http/coreos-download.sh
+++ http/coreos-download.sh
@@ -1,23 +1,28 @@
 # global vars
 # dirname command is using script dir regardless of where it is executed from
 BASE_DIR=$(dirname -- "$0")
-
-# tail -n +2 reads all lines starting on 2 (excluders headers)
+COREOS_BASE="${BASE_DIR}/httpd/coreos"
 
 # loop through builds file, for each line download
-tail -n +2 $BASE_DIR/coreos-builds.csv | while read line
-  do 
-    # set some vars
-    CURRENT_LINE=$(echo $line)
-    CHANNEL=$(echo $CURRENT_LINE | awk -F ',' '{print $1}')
-    BUILD=$(echo $CURRENT_LINE | awk -F ',' '{print $2}')
-    
-    # test messagge
-    echo "channel is ${CHANNEL} build is ${BUILD}"
-    
-    # ensure download dir exists
-    mkdir -p $BASE_DIR/httpd/coreos/$CHANNEL
+# tail -n +2 reads all lines starting on 2 (excluders headers)
+tail -n +2 $BASE_DIR/coreos-builds.csv | while read line; do
+  # set some vars
+  CURRENT_LINE=$(echo $line)
+  CHANNEL=$(echo $CURRENT_LINE | awk -F ',' '{print $1}')
+  BUILD=$(echo $CURRENT_LINE | awk -F ',' '{print $2}')
+  COREOS_SOURCE="https://${CHANNEL}.release.core-os.net/amd64-usr/${BUILD}"
 
-    # check for files and download if they don't exist
-    #if [ !f ]
+  # test messagge
+  echo "channel is ${CHANNEL} build is ${BUILD}"
+
+  # ensure download dir exists
+  mkdir -p $COREOS_BASE/$CHANNEL/$BUILD
+
+  # check for files and download if they don't exist
+  FILES="coreos_production_pxe.vmlinuz coreos_production_pxe_image.cpio.gz"
+  for FILE in $FILES; do
+    if [ ! -f $COREOS_BASE/$CHANNEL/$BUILD/$FILE ]; then
+      curl -L $COREOS_SOURCE/$FILE -o $COREOS_BASE/$CHANNEL/$BUILD/$FILE
+    fi
   done
+done
